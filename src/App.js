90f859693f08e2c329c5a0476831371058179b0f
Max-Rus1977 <maxsvet77@yandex.ru>
2022-12-01 16:16:09 +0300

    Реализация пагинации с кнопками prev и next

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import './style.css';
 
 function App() {
+
+  const [posts, setPosts] = useState([])
+  const [totalPostsCount, setTotalPostsCount] = useState(0)
+  const [limit] = useState(10)
+  const [page, setPage] = useState(1)
+
+  useEffect(() => {
+    const getPostsServer = async () => {
+      const responseServer = await axios.get('https://jsonplaceholder.typicode.com/posts', {
+        params: {
+          _limit: limit,
+          _page: page
+        }
+      })
+      return (
+        setTotalPostsCount(+responseServer.headers['x-total-count']),
+        setPosts(responseServer.data)
+      )
+    }
+    getPostsServer()
+  }, [page])
+
+  const totalPagesCount = Math.ceil(totalPostsCount / limit)
+
+  const arrTotalPages = []
+  for (let i = 0; i < totalPagesCount; i++) {
+    arrTotalPages.push(i + 1)
+  }
+
+  const goToPage = (numPage) => {
+    setPage(numPage)
+  }
+
+  const goToPrev = () => {
+    if (page > 1) {
+      setPage(page - 1)
+    }
+  }
+
+  const goToNext = () => {
+    if (page < totalPagesCount) {
+      setPage(page + 1)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>Пагинация React</h1>
+      <div className='box-posts'>
+        {
+          posts.map((post) =>
+            <div key={post.id} className='post'>
+              <h3>{post.id}. {post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          )
+        }
+        <div className='box-pagination'>
+          <button onClick={goToPrev} className='buttonsPN btn-prev'>⊲</button>
+          {
+            arrTotalPages.map((numPage) =>
+              <button
+                key={numPage}
+                className={numPage === page ? 'btn active' : 'btn'}
+                onClick={() => goToPage(numPage)}
+              >
+                {numPage}
+              </button>
+            )
+          }
+          <button onClick={goToNext} className='buttonsPN btn-next'>⊳</button>
+        </div>
+      </div>
+    </div >
   );
 }
 
